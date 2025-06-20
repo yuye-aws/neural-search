b3b46cf81fa35d1de7416eb644ea6be6d7100059
yuye-aws <yuyezhu@amazon.com>
2025-06-20 12:05:36 +0800

    Remove unused import
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>

--- src/main/java/org/opensearch/neuralsearch/sparse/algorithm/RandomClustering.java
+++ src/main/java/org/opensearch/neuralsearch/sparse/algorithm/RandomClustering.java
@@ -12,7 +12,6 @@ import org.opensearch.neuralsearch.sparse.common.SparseVectorReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.IntStream;
 
 /**
