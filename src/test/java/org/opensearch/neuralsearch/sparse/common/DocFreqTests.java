c13b459d7d57a2f445b175fa4c6cf989b32afaaa
Zirui Song <zrsong@amazon.com>
2025-07-09 17:57:39 +0800

    Update to be more simple in one single test case
    Signed-off-by: Zirui Song <zrsong@amazon.com>

--- src/test/java/org/opensearch/neuralsearch/sparse/common/DocFreqTests.java
+++ src/test/java/org/opensearch/neuralsearch/sparse/common/DocFreqTests.java
@@ -8,83 +8,106 @@ import org.opensearch.neuralsearch.sparse.AbstractSparseTestBase;
 
 public class DocFreqTests extends AbstractSparseTestBase {
 
-    public void testConstructorAndGetters() {
+    public void testGetDocID_afterConstruction_returnsCorrectValue() {
         int docID = 42;
         byte freq = 5;
         DocFreq docFreq = new DocFreq(docID, freq);
-
         assertEquals(docID, docFreq.getDocID());
+    }
+
+    public void testGetFreq_afterConstruction_returnsCorrectValue() {
+        int docID = 42;
+        byte freq = 5;
+        DocFreq docFreq = new DocFreq(docID, freq);
         assertEquals(freq, docFreq.getFreq());
     }
 
-    public void testGetIntFreq() {
-        // Test positive byte value
-        DocFreq docFreq1 = new DocFreq(1, (byte) 5);
-        assertEquals(5, docFreq1.getIntFreq());
-
-        // Test byte value that would be negative in signed interpretation
-        DocFreq docFreq2 = new DocFreq(2, (byte) 0xFF); // -1 as signed byte, 255 as unsigned
-        assertEquals(255, docFreq2.getIntFreq());
-
-        // Test zero value
-        DocFreq docFreq3 = new DocFreq(3, (byte) 0);
-        assertEquals(0, docFreq3.getIntFreq());
-
-        // Test max positive signed byte value
-        DocFreq docFreq4 = new DocFreq(4, (byte) 127);
-        assertEquals(127, docFreq4.getIntFreq());
+    public void testGetIntFreq_withPositiveByteValue_returnsCorrectValue() {
+        DocFreq docFreq = new DocFreq(1, (byte) 5);
+        assertEquals(5, docFreq.getIntFreq());
     }
 
-    public void testCompareTo() {
+    public void testGetIntFreq_withNegativeByteValue_returnsUnsignedValue() {
+        DocFreq docFreq = new DocFreq(2, (byte) 0xFF); // -1 as signed byte, 255 as unsigned
+        assertEquals(255, docFreq.getIntFreq());
+    }
+
+    public void testGetIntFreq_withZeroValue_returnsZero() {
+        DocFreq docFreq = new DocFreq(3, (byte) 0);
+        assertEquals(0, docFreq.getIntFreq());
+    }
+
+    public void testGetIntFreq_withMaxSignedByteValue_returnsCorrectValue() {
+        DocFreq docFreq = new DocFreq(4, (byte) 127);
+        assertEquals(127, docFreq.getIntFreq());
+    }
+
+    public void testCompareTo_withSmallerDocID_returnsNegative() {
         DocFreq docFreq1 = new DocFreq(1, (byte) 10);
         DocFreq docFreq2 = new DocFreq(2, (byte) 5);
-        DocFreq docFreq3 = new DocFreq(1, (byte) 20);
-
-        // Test less than
         assertTrue(docFreq1.compareTo(docFreq2) < 0);
-
-        // Test greater than
-        assertTrue(docFreq2.compareTo(docFreq1) > 0);
-
-        // Test equal docIDs (frequency should not affect comparison)
-        assertEquals(0, docFreq1.compareTo(docFreq3));
     }
 
-    public void testEqualsAndHashCode() {
+    public void testCompareTo_withLargerDocID_returnsPositive() {
+        DocFreq docFreq1 = new DocFreq(2, (byte) 5);
+        DocFreq docFreq2 = new DocFreq(1, (byte) 10);
+        assertTrue(docFreq1.compareTo(docFreq2) > 0);
+    }
+
+    public void testCompareTo_withSameDocID_returnsZero() {
+        DocFreq docFreq1 = new DocFreq(1, (byte) 10);
+        DocFreq docFreq2 = new DocFreq(1, (byte) 20);
+        assertEquals(0, docFreq1.compareTo(docFreq2));
+    }
+
+    public void testEquals_withSameValues_returnsTrue() {
         DocFreq docFreq1 = new DocFreq(1, (byte) 10);
         DocFreq docFreq2 = new DocFreq(1, (byte) 10);
-        DocFreq docFreq3 = new DocFreq(1, (byte) 20);
-        DocFreq docFreq4 = new DocFreq(2, (byte) 10);
-
-        // Test equality with same values
         assertEquals(docFreq1, docFreq2);
-        assertEquals(docFreq1.hashCode(), docFreq2.hashCode());
-
-        // Test inequality with different freq
-        assertNotEquals(docFreq1, docFreq3);
-
-        // Test inequality with different docID
-        assertNotEquals(docFreq1, docFreq4);
-
-        // Test inequality with null
-        assertNotEquals(docFreq1, null);
-
-        // Test inequality with different type
-        assertNotEquals(docFreq1, "not a DocFreq");
     }
 
-    public void testCompareToEdgeCases() {
+    public void testEquals_withDifferentFreq_returnsFalse() {
+        DocFreq docFreq1 = new DocFreq(1, (byte) 10);
+        DocFreq docFreq2 = new DocFreq(1, (byte) 20);
+        assertNotEquals(docFreq1, docFreq2);
+    }
+
+    public void testEquals_withDifferentDocID_returnsFalse() {
+        DocFreq docFreq1 = new DocFreq(1, (byte) 10);
+        DocFreq docFreq2 = new DocFreq(2, (byte) 10);
+        assertNotEquals(docFreq1, docFreq2);
+    }
+
+    public void testEquals_withNull_returnsFalse() {
+        DocFreq docFreq = new DocFreq(1, (byte) 10);
+        assertNotEquals(docFreq, null);
+    }
+
+    public void testEquals_withDifferentType_returnsFalse() {
+        DocFreq docFreq = new DocFreq(1, (byte) 10);
+        assertNotEquals(docFreq, "not a DocFreq");
+    }
+
+    public void testHashCode_withEqualObjects_returnsSameValue() {
+        DocFreq docFreq1 = new DocFreq(1, (byte) 10);
+        DocFreq docFreq2 = new DocFreq(1, (byte) 10);
+        assertEquals(docFreq1.hashCode(), docFreq2.hashCode());
+    }
+
+    public void testCompareTo_withMinAndMaxValues_returnsNegative() {
         DocFreq minDocFreq = new DocFreq(Integer.MIN_VALUE, (byte) 0);
         DocFreq maxDocFreq = new DocFreq(Integer.MAX_VALUE, (byte) 0);
-
-        // Test min vs max
         assertTrue(minDocFreq.compareTo(maxDocFreq) < 0);
+    }
 
-        // Test max vs min
+    public void testCompareTo_withMaxAndMinValues_returnsPositive() {
+        DocFreq minDocFreq = new DocFreq(Integer.MIN_VALUE, (byte) 0);
+        DocFreq maxDocFreq = new DocFreq(Integer.MAX_VALUE, (byte) 0);
         assertTrue(maxDocFreq.compareTo(minDocFreq) > 0);
+    }
 
-        // Test self comparison
-        assertEquals(0, minDocFreq.compareTo(minDocFreq));
-        assertEquals(0, maxDocFreq.compareTo(maxDocFreq));
+    public void testCompareTo_withSameObject_returnsZero() {
+        DocFreq docFreq = new DocFreq(1, (byte) 10);
+        assertEquals(0, docFreq.compareTo(docFreq));
     }
 }
