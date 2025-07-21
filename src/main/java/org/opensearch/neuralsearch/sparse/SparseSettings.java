9e573e3bba4d053e6c3150f91526842c77638aed
yuye-aws <yuyezhu@amazon.com>
2025-07-21 16:58:02 +0800

    Remove profiling and sparse memory index settings (#87)
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>

--- src/main/java/org/opensearch/neuralsearch/sparse/SparseSettings.java
+++ src/main/java/org/opensearch/neuralsearch/sparse/SparseSettings.java
@@ -14,7 +14,6 @@ import static org.opensearch.common.settings.Setting.Property.IndexScope;
  */
 public class SparseSettings {
     public static final String SPARSE_INDEX = "index.sparse";
-    public static final String MEMORY_USAGE = "index.sparse.memory";
 
     private static SparseSettings INSTANCE;
 
@@ -29,13 +28,4 @@ public class SparseSettings {
      * This setting identifies sparse index.
      */
     public static final Setting<Boolean> IS_SPARSE_INDEX_SETTING = Setting.boolSetting(SPARSE_INDEX, false, IndexScope, Final);
-    /**
-     * This is just a fake setting for POC whenever it changes value, we output the memory usage of the in-memory data
-     */
-    public static final Setting<Boolean> SPARSE_MEMORY_SETTING = Setting.boolSetting(
-        MEMORY_USAGE,
-        false,
-        IndexScope,
-        Setting.Property.Dynamic
-    );
 }
