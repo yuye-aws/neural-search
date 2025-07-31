a98b0412b169cd0d76ecc349523ba7d53945aa55
zirui-song-18 <zrsong@amazon.com>
2025-07-31 13:47:22 +0800

    Remove Lambda from The Input List (#66)
    * A feature: removed lambda from the input list, now dynamically changing nPostings according to the dataset size
    
    Signed-off-by: Zirui Song <zrsong@amazon.com>
    
    * New logic: dynamically decide pruned posting list length if user does not give a value
    
    Signed-off-by: Zirui Song <zrsong@amazon.com>
    
    * Add a comment for DEFAULT_N_POSTINGS = -1
    
    Signed-off-by: Zirui Song <zrsong@amazon.com>
    
    * Modify according to Yuye's comments
    
    Signed-off-by: Zirui Song <zrsong@amazon.com>
    
    * update PR according to comments: sync all with DEFAULT_N_POSTINS
    
    Signed-off-by: Zirui Song <zrsong@amazon.com>
    
    ---------
    
    Signed-off-by: Zirui Song <zrsong@amazon.com>

--- src/main/java/org/opensearch/neuralsearch/sparse/common/SparseConstants.java
+++ src/main/java/org/opensearch/neuralsearch/sparse/common/SparseConstants.java
@@ -13,10 +13,18 @@ public final class SparseConstants {
     public static final String CLUSTER_RATIO_FIELD = "cluster_ratio";
     public static final String APPROXIMATE_THRESHOLD_FIELD = "approximate_threshold";
 
+    /**
+     * DEFAULT_APPROXIMATE_THRESHOLD: Minimum document collection number to trigger Seismic
+     * DEFAULT_POSTING_MINIMUM_LENGTH: Minimum posting length when operating with a Seismic segments
+     * SEISMIC is generally only recommended for collections at least 100K documents, where 160 is a good value for the posting length
+     * If N_POSTINGS is not provided, each posting list will be automatically pruned with DEFAULT_POSTING_PRUNE_RATIO
+     */
     public static final class Seismic {
-        public static final int DEFAULT_N_POSTINGS = 6000;
+        public static final int DEFAULT_N_POSTINGS = -1;
         public static final float DEFAULT_SUMMARY_PRUNE_RATIO = 0.4f;
         public static final float DEFAULT_CLUSTER_RATIO = 0.1f;
         public static final int DEFAULT_APPROXIMATE_THRESHOLD = 1000000;
+        public static final float DEFAULT_POSTING_PRUNE_RATIO = 0.0005f;
+        public static final int DEFAULT_POSTING_MINIMUM_LENGTH = 160;
     }
 }
